1cd97df38d72cfeb87856ab7d504f15b793d3fa5
Jose Luis Rodriguez Contreras <jl.rodriguez@globant.com>
2018-08-25 14:10:44 -0500

    Adding WIP for pager of add employee.

--- app/src/main/java/com/ergomotions/activity/AddEmployeeActivity.kt
+++ app/src/main/java/com/ergomotions/activity/AddEmployeeActivity.kt
@@ -1,7 +1,33 @@
 package com.ergomotions.activity
 
+import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuItem
+import com.ergomotions.R
+import com.ergomotions.adapter.SimpleFragmentPagerAdapter
+import kotlinx.android.synthetic.main.activity_add_employee.*
 
 class AddEmployeeActivity : AppCompatActivity() {
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_employee)
+        viewpager.adapter = SimpleFragmentPagerAdapter(this, supportFragmentManager)
+        tabs.setupWithViewPager(viewpager)
+        toolbar.title = getString(R.string.add_employee)
+        setSupportActionBar(toolbar)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        when (item?.itemId) {
+
+        }
+        return super.onOptionsItemSelected(item)
+    }
 }
--- app/src/main/java/com/ergomotions/activity/EmployeeMainActivity.kt
+++ app/src/main/java/com/ergomotions/activity/EmployeeMainActivity.kt
@@ -4,12 +4,18 @@ import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import com.ergomotions.R
+import kotlinx.android.synthetic.main.activity_employee_main.*
 
 class EmployeeMainActivity: AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_employee_main)
+
+        employeeAdd.setOnClickListener {
+            startActivity(Intent(applicationContext, AddEmployeeActivity::class.java))
+        }
+
     }
 
     override fun onBackPressed() {
