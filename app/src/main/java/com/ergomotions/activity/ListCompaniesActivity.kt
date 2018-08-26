f1859e6812ac4fe3b3423c44dc3c8dee6d2c9aad
Jose Luis Rodriguez Contreras <jl.rodriguez@globant.com>
2018-08-26 13:12:52 -0500

    Company list done.

--- app/src/main/java/com/ergomotions/activity/ListCompaniesActivity.kt
+++ app/src/main/java/com/ergomotions/activity/ListCompaniesActivity.kt
@@ -4,16 +4,18 @@ import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import com.ergomotions.R
-import kotlinx.android.synthetic.main.activity_employee_main.*
+import android.support.v7.widget.LinearLayoutManager
+import com.ergomotions.adapter.SingleListAdapter
+import kotlinx.android.synthetic.main.activity_list_companies.*
 
-class EmployeeMainActivity: AppCompatActivity() {
+
+class ListCompaniesActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_employee_main)
-        employeeAdd.setOnClickListener {
-            startActivity(Intent(applicationContext, AddEmployeeActivity::class.java))
-        }
+        setContentView(R.layout.activity_list_companies)
+        companiesList.layoutManager = LinearLayoutManager(this)
+        companiesList.adapter = SingleListAdapter(this)
     }
 
     override fun onBackPressed() {
